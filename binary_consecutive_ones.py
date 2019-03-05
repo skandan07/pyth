66583448fac7d3c5e7df0cd21750a27d6c73a985
Gnanaskandan V <48236773+skandan07@users.noreply.github.com>
2019-03-05 18:47:40 +0530

    Update binary_consecutive_ones.py

--- binary_consecutive_ones.py
+++ binary_consecutive_ones.py
@@ -1,4 +1,16 @@
-x = "110"
+d=742
+b=[]
+while d!=0:
+    if d%2==1:
+        b.append("1")
+    else:
+        b.append("0")
+    d//2
+
+b.reverse()
+print(b)
+x="".join(b)
+print(x)
 l=len(x)
 s=0
 s_max=0
